10728243c9347b2955c12d8e2b222064464c5fdb
Mahedi <mahedi0244@gmail.com>
2016-03-13 11:55:42 -0400

    Add Funtion to Class

---
+++ CPU.java
@@ -0,0 +1,120 @@
+
+
+public class CPU {
+	private boolean available;
+	private int cycle;
+    CPU (){
+    	setAvailable(true);
+    	setCycle(0);
+    }
+	public boolean isAvailable() {
+		return available;
+	}
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+	
+	public int getCycle() {
+		return cycle;
+	}
+	public void setCycle(int cycle) {
+		this.cycle = cycle;
+	}
+
+	public void Run(JobQueue myJobQueue, ReadyQueue myReadyQueue, BlockedQueue myBlockedQueue){
+		System.out.println("inside Run method of CPU class");
+		System.out.println("After time cycle of "+cycle);
+		System.out.println("printing myReadyQueue: "+myReadyQueue);
+		System.out.println("printing myBlockedQueue: "+myBlockedQueue);
+		
+		
+		while(true){
+			
+			//checking if ReadyQueue is empty or not
+			if (!myReadyQueue.isEmpty()){
+				
+				System.out.println("(!myReadyQueue.isEmpty())");
+				PCB currentPCB = myReadyQueue.dequeue();
+				
+				int currentCPUBurst = currentPCB.getCurrentCPUBurst();
+				
+				//checking if current CPUBurst is the last CPUBurst or not
+				if (currentCPUBurst >= currentPCB.getTotalCPUBursts()){
+					
+					System.out.println("(currentCPUBurst >= currentPCB.getTotalCPUBursts()");
+					
+					
+					//System.out.println("myReadyQueue"+myReadyQueue);
+					//System.out.println("myBlockedQueue"+myBlockedQueue);
+					//System.out.println(myJobQueue.isEmpty());
+					
+					if (myReadyQueue.isEmpty() && myJobQueue.isEmpty() && myBlockedQueue.isEmpty()){
+						System.out.println("myReadyQueue.isEmpty() && myJobQueue.isEmpty() && myBlockedQueue.isEmpty())");
+					}
+					else if (myReadyQueue.isEmpty() && !myJobQueue.isEmpty()){
+						System.out.println("(myReadyQueue.isEmpty() && !myJobQueue.isEmpty())");
+						myReadyQueue.enqueue(myJobQueue.getNextJob());
+						currentPCB = myReadyQueue.dequeue();
+						currentCPUBurst = currentPCB.getCurrentCPUBurst();
+					}
+					else if (myReadyQueue.isEmpty() && myJobQueue.isEmpty() && !myBlockedQueue.isEmpty()){
+						System.out.print("I am here    kjshkdhahd");
+						
+						int waitTime = myBlockedQueue.peek().getBlockedEndTime()-cycle;
+						cycle += waitTime;
+						myBlockedQueue.BlockedTimer(cycle,myReadyQueue);
+						
+					}
+				}
+				if (currentCPUBurst < currentPCB.getTotalCPUBursts()){
+					System.out.println("(currentCPUBurst < currentPCB.getTotalCPUBursts())");
+					cycle += currentPCB.getCPUBursts()[currentCPUBurst];
+					currentPCB.setCurrentCPUBurst(currentCPUBurst+1);
+				
+					
+					if (currentPCB.getCurrentCPUBurst() >= currentPCB.getTotalCPUBursts()){
+						System.out.println("(currentCPUBurst >= currentPCB.getTotalCPUBursts()");
+						
+						
+						System.out.println("myReadyQueue"+myReadyQueue);
+						System.out.println("myBlockedQueue"+myBlockedQueue);
+						System.out.println(myJobQueue.isEmpty());
+						
+						if (myReadyQueue.isEmpty() && myJobQueue.isEmpty() && myBlockedQueue.isEmpty()){
+							System.out.println("myReadyQueue.isEmpty() && myJobQueue.isEmpty() && myBlockedQueue.isEmpty())");
+							return;
+						}
+						else if (myReadyQueue.isEmpty() && !myJobQueue.isEmpty()){
+							System.out.println("(myReadyQueue.isEmpty() && !myJobQueue.isEmpty())");
+							myReadyQueue.enqueue(myJobQueue.getNextJob());
+							currentPCB = myReadyQueue.dequeue();
+							currentCPUBurst = currentPCB.getCurrentCPUBurst();
+						}
+						else if (myReadyQueue.isEmpty() && myJobQueue.isEmpty() && !myBlockedQueue.isEmpty()){
+							System.out.print("I am here    kjshkdhahd");
+							
+							int waitTime = myBlockedQueue.peek().getBlockedEndTime()-cycle;
+							cycle += waitTime;
+							myBlockedQueue.BlockedTimer(cycle,myReadyQueue);
+							
+						}
+					}
+					
+					currentPCB.setBlockedStartTime(cycle);
+					currentPCB.setBlockedEndTime(cycle+10);
+					myBlockedQueue.enqueue(currentPCB);
+					myBlockedQueue.BlockedTimer(cycle,myReadyQueue);
+				
+					System.out.println("\nAfter time cycle of "+cycle);
+					System.out.println("printing myReadyQueue: "+myReadyQueue);
+					System.out.println("printing myBlockedQueue: "+myBlockedQueue);
+				}
+			}
+			if (myReadyQueue.isEmpty() && !myBlockedQueue.isEmpty()){
+				int waitTime = myBlockedQueue.peek().getBlockedEndTime()-cycle;
+				cycle += waitTime;
+				myBlockedQueue.BlockedTimer(cycle,myReadyQueue);
+			}
+		}
+	}
+}
---
+++ PCB.java
@@ -0,0 +1,135 @@
+
+public class PCB {
+	private int jobId;
+	private int arrivalTime;
+	private int cycleCounter;
+	private int currentCPUBurst;
+	private int totalCPUBursts;
+	private int[] CPUBursts;
+	private int completionTime;
+	private int IOCompletionTime;
+	private String state;
+	
+	private int blockedStartTime;
+	private int blockedEndTime;
+	
+	PCB(String jobDescription ){
+		
+		String []tempArray = jobDescription.split("\\s");
+		
+		jobId = Integer.valueOf(tempArray[0]);
+		arrivalTime = Integer.valueOf(tempArray[1]);
+		state = null;
+		
+		cycleCounter = 0;
+		blockedStartTime = 0;
+		blockedEndTime = 0;
+		setCurrentCPUBurst(0);
+		
+		totalCPUBursts = Integer.valueOf(tempArray[2]);
+		
+		
+		CPUBursts = new int [totalCPUBursts];
+		for (int i = 0; i<totalCPUBursts; i++){
+			CPUBursts[i] = Integer.valueOf(tempArray[i+3]);
+			
+;		}
+	}
+
+	public int getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(int jobId) {
+		this.jobId = jobId;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getCycleCounter() {
+		return cycleCounter;
+	}
+
+	public void setCycleCounter(int cycleCounter) {
+		this.cycleCounter = cycleCounter;
+	}
+
+	public int getTotalCPUBursts() {
+		return totalCPUBursts;
+	}
+
+	public void setTotalCPUBursts(int totalCPUBursts) {
+		this.totalCPUBursts = totalCPUBursts;
+	}
+
+	public int[] getCPUBursts() {
+		return CPUBursts;
+	}
+
+	public void setCPUBursts(int[] cPUBursts) {
+		CPUBursts = cPUBursts;
+	}
+
+	public int getCompletionTime() {
+		return completionTime;
+	}
+
+	public void setCompletionTime(int completionTime) {
+		this.completionTime = completionTime;
+	}
+
+	public int getIOCompletionTime() {
+		return IOCompletionTime;
+	}
+
+	public void setIOCompletionTime(int iOCompletionTime) {
+		IOCompletionTime = iOCompletionTime;
+	}
+	
+	public int getCurrentCPUBurst() {
+		return currentCPUBurst;
+	}
+
+	public void setCurrentCPUBurst(int currentCPUBurst) {
+		this.currentCPUBurst = currentCPUBurst;
+	}
+	
+	public int getBlockedStartTime() {
+		return blockedStartTime;
+	}
+
+	public void setBlockedStartTime(int blockedStartTime) {
+		this.blockedStartTime = blockedStartTime;
+	}
+
+	public int getBlockedEndTime() {
+		return blockedEndTime;
+	}
+
+	public void setBlockedEndTime(int blockedEndTime) {
+		this.blockedEndTime = blockedEndTime;
+	}
+
+	
+	public String toString(){
+		String myString = "";
+		myString+="\n\nCurrent information of this PCB\n\n";
+		myString+="jobId = "+jobId+"\narrivalTime = "+arrivalTime+"\ncycleCounter = "+cycleCounter;
+		
+		myString+="\ncurrentCPUBurst = "+currentCPUBurst+" \ntotalCPUBursts = "+totalCPUBursts+"\nCPUBursts : ";
+		for (int i = 0; i<totalCPUBursts; i++)
+			myString+=" "+ CPUBursts[i];
+		
+		myString+="\ncompletionTime = "+completionTime+"\nIOCompletionTime = "+IOCompletionTime+"\nstate = "+state;
+		
+		return myString;
+	}
+
+	
+}
