fab54d4fe1c71cf1b903ff22c8b480b035ac2aeb
Jason <jasonrfarkas@gmail.com>
2016-03-30 23:07:13 -0400

    deleted comments in Readyq

--- ReadyQueue.java
+++ ReadyQueue.java
@@ -2,21 +2,7 @@
 */
 
 public class ReadyQueue extends Queue{
-   /*
-    * This method can be used to print all the pcb informations that are in the ReadyQueue
-    * 
-   public String toString(){
-    	
-    	String myString ="";
-    	Node walker = this.head;
-    	while (walker!= null){
-    		myString += walker.getPcb();
-    		walker = walker.getNext();
-    	}
-    	return myString;
-    }
-    
-    */
+
     
 }
 
