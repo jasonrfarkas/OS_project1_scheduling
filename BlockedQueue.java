d87137766e3fa4c02386d91b7fb65de497a6b9f8
Mahedi <mahedi0244@gmail.com>
2016-03-30 21:52:17 -0400

    doing the comments

--- BlockedQueue.java
+++ BlockedQueue.java
@@ -2,16 +2,14 @@
 
 public class BlockedQueue extends Queue{
     
-    public Queue filterBlockedQueue(Queue myQ){/*int currentCycleTime,*/
+    public Queue filterBlockedQueue(Queue myQ){
 		
-         // System.out.println(" BlockedeQueue string:" +this.toString());
-		 // System.out.println("inside Blockedtimer method of BlockedeQueue + size:" +size());
 		Node walker = this.head;
     	while (walker!= null && walker.getPcb().recheckIfWaiting() == 0){
             myQ.enqueue(this.dequeue());
             walker = walker.getNext();
 		}
-        // System.out.println("ending Blockedtimer method of BlockedeQueue");
+        
         return myQ;
 	}
 
